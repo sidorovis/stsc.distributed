4613236b8441ad9c02373811f5e6b2e56d748684
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-11-01 20:46:12 +0100

    removed meaningless TODO from HadoopStarter

--- hadoop/src/main/java/stsc/distributed/hadoop/grid/HadoopStarter.java
+++ hadoop/src/main/java/stsc/distributed/hadoop/grid/HadoopStarter.java
@@ -6,10 +6,9 @@ import stsc.general.strategy.TradingStrategy;
 
 /**
  * Simple interface for Hadoop starter. <br/>
- * TODO should be modified and improved.
  */
 public interface HadoopStarter {
 
-	public List<TradingStrategy> searchOnHadoop() throws Exception;
+	List<TradingStrategy> searchOnHadoop() throws Exception;
 
 }
