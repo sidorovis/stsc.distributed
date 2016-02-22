af825e944a0c5f2ae1bd1c7ecb899609fffdded5
Ivan Sidarau <ivan.sidarau@gmail.com>
2016-02-23 00:42:38 +0300

    added initial GeneticSparkStarter test

---
+++ spark/src/test/java/stsc/distributed/spark/grid/GeneticSparkStarterTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package stsc.distributed.spark.grid;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GeneticSparkStarterTest {
+
+	@Test
+	public void testGeneticSparkStarter() {
+		
+		fail("Not yet implemented");
+	}
+
+}
