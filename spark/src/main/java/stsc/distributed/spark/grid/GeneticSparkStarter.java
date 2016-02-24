a146e88819aa093109e77892c26092bf51d65fde
Ivan Sidarau <ivan.sidarau@gmail.com>
2016-02-25 00:45:33 +0300

    added GeneticSparkStarter

---
+++ spark/src/main/java/stsc/distributed/spark/grid/GeneticSparkStarter.java
@@ -0,0 +1,21 @@
+package stsc.distributed.spark.grid;
+
+import java.util.Arrays;
+import java.util.List;
+
+import stsc.general.strategy.TradingStrategy;
+
+/**
+ * This is a default starter for genetic on spark search.
+ */
+public final class GeneticSparkStarter {
+	
+	public GeneticSparkStarter() {
+		
+	}
+	
+	public List<TradingStrategy> searchOnSpark() {
+		return Arrays.asList();
+	}
+	
+}
--- spark/src/test/java/stsc/distributed/spark/grid/GeneticSparkStarterTest.java
+++ spark/src/test/java/stsc/distributed/spark/grid/GeneticSparkStarterTest.java
@@ -3,13 +3,20 @@
  */
 package stsc.distributed.spark.grid;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import stsc.general.strategy.TradingStrategy;
 
 public class GeneticSparkStarterTest {
 
 	@Test
 	public void testGeneticSparkStarter() {
-		
+		final GeneticSparkStarter geneticSparkStarter = new GeneticSparkStarter();
+		final List<TradingStrategy> searchOnSpark = geneticSparkStarter.searchOnSpark();
+		Assert.assertTrue(searchOnSpark.isEmpty());
 	}
 
 }
